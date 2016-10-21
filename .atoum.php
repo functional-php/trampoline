f5c98a73366e5c1c6b954b099d7c7adbb1879395
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-10-21 21:46:48 +0200

    code coverage

--- .atoum.php
+++ .atoum.php
@@ -7,4 +7,9 @@ $report = $script->addDefaultReport();
 $coverageField = new atoum\report\fields\runner\coverage\html('Trampoline', './reports/');
 $report->addField($coverageField);
 
+$cloverWriter = new atoum\writers\file('./reports/atoum.coverage.xml');
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+$runner->addReport($cloverReport);
+
 $runner->addTestsFromDirectory('./tests');
